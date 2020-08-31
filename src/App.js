c62443e5c269b2e7051d5ed4a5498e85c9e43e98
CraigStueber <Craig@kurbmedia.com>
2020-08-31 05:49:32 -0400

    got palette set up

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
-import './App.css';
+import Pallette from './Palette';
+import seedColors from './SeedColor';
+
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello World!</h1>
+      <Pallette {...seedColors[]} />
     </div>
   );
 }
---
+++ src/Palette.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+class Pallette extends Component{
+    render(){
+        return(
+            <div><h1>YAY I am coming from Pallette</h1> </div>
+        )
+    }
+}
+
+export default Pallette;
