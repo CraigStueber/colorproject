62918a7f5d4fe785e34a92da2e99258bb2083d47
CraigStueber <Craig@kurbmedia.com>
2020-09-01 05:59:48 -0400

    slide bar styled

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
 
 import React, { Component } from "react";
 import Palette from "./Palette";
-import seedColors from "./seedColors";
+import seedColors from "./SeedColors";
 import { generatePalette } from "./colorHelpers";
 
 class App extends Component {
   render() {
-    console.log(generatePalette(seedColors[4]));
     return (
       <div>
-        <Palette {...seedColors[4]} />
+        <Palette palette={generatePalette(seedColors[4])} />
       </div>
     );
   }
